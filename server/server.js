e04507ae1bdc1e6e8e3bf3ecc09b0c5c4a8cd566
Bj Aberle <bjaberle@ionagroup.com>
2017-09-05 12:52:35 -0500

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -22,12 +22,11 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-	var todo = new Todo;
 	Todo.find().then((todos) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
-	})
+	});
 });
 
 app.listen(3001, () => {
